6c1aa7d8ef09d19bfb3aee849d2b01d807851be6
Bohdan Domalchuk <bohdan.domalchuk@vitechteam.com>
2021-04-12 02:01:53 +0300

    Build CI/CD

---
+++ src/main/java/ua/domaly/lwserver/LwServerApplication.java
@@ -0,0 +1,13 @@
+package ua.domaly.lwserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LwServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LwServerApplication.class, args);
+    }
+
+}
